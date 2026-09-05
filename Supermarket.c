6c9ee950e1054c85c8cac4b48dbcac216fcd937e
skualisk <aarron.jona.dcruz@gmail.com>
2026-09-06 00:40:14 +0530

    Update Supermarket.c
    Added "Add to Cart" (Could add one more feature) and "Remove From Cart" function

--- Supermarket.c
+++ Supermarket.c
@@ -1,9 +1,75 @@
 /*Supermarket management system, by Aarron Dcruz 2661123, Anshuman Singh 2661126, 
   Yankit Gupta 2661144 and Uneeth Singh 2661183*/
+#import <stdio.h>
 
+struct Cart //Bro struct is like a reusable variable thing
+{
+  char pname[50];
+  float price;
+  float quan;
+}
 
+void AddCart();
+
+void RemoveCart();
 
 int main()
 {
 
+}
+
+void AddCart()
+{
+  struct Cart Ad;
+  printf("Enter Product Name: ");
+  gets(Ad.pname);
+  printf("Enter Price of Product");  // Am thinking we read this from our inventory.csv file but little complex
+  scanf("%f", &Ad.price);
+  printf("Enter Quantity of Product");
+  scanf("%f", &Ad.quan)
+
+  FILE *fad = fopen("Cart.csv", "a");
+  if (fad == NULL)
+  {
+    printf("File not Found");
+    exit(0);
+  }
+  fprintf(fad, %s, %.4f, %.2f, Ad.pname, Ad.price, Ad.quan);   // Another thinng, do we add tax per item or tax the whole thing
+  printf("-------------- ITEM ADDED SUCCESSFULLY --------------");
+
+  f.close(fad);
+}
+
+void RemoveCart()
+{
+  char IName[50];
+  printf("Enter Item Name to be removed:\t");
+  gets(IName);
+  File *fde = fopen("Cart.csv", r);
+  File *ftemp = fopen("Temp.csv", a);
+  char line[256];
+  int found;
+  while (fgets(line, sizeof[line], fde) != NULL)
+  {
+    line(strcspn(line, "\n")) = 0;
+    char *name = strok(line, ",");
+    char *price = strok(NULL, ",");
+    char *quan = strok(NULL, ",");
+    if (strcmp(IName, *name) == 0)
+    {
+      found = 1;
+      continue;
+    }
+    fprintf(ftemp, %s, %s, %s, *name, *price, *quan);
+  }
+  fclose(fde);
+  fclose(ftemp);
+  if (found == 1)
+  {
+    printf("Item Not Found");
+  }
+  else
+    printf("----------------- ITEM DELETED FROM CART -----------------");
+  remove ("Cart.csv");
+  rename ("Temp.csv", "Cart.csv");
 }
